cba4752c764de3949a64b465721dcac1cad1d9d8
Edward Johnson <88259575+EdwardSJ151@users.noreply.github.com>
2022-08-06 23:16:06 -0300

    Add files via upload
    Ex 6 e 7 do curso de Python do Gustavo Guanabara.

---
+++ ex006.py
@@ -0,0 +1,6 @@
+n1=int(input('Escreva um número inteiro: '))
+dob=n1*2
+trip=n1*3
+raiz=n1**(1/2)
+
+print(f'O dobro de {n1} é {dob}, o seu triplo é {trip}, e a sua raiz quadrada é {raiz:.2}.')
---
+++ ex007.py
@@ -0,0 +1,4 @@
+n1=float(input('Vamos cáluclar sua nota média. Digite a primeira nota: '))
+n2=float(input('Digite a segunda nota: '))
+med=(n1+n2)/2
+print(f'A sua média é {med:1f}.')
